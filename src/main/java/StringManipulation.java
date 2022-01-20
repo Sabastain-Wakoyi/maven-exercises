99cb27283fb8c77e3c35d99f336e75793cbe6c77
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-01-20 15:19:17 -0600

    maven-exercises

--- src/main/java/StringManipulation.java
+++ src/main/java/StringManipulation.java
@@ -18,7 +18,24 @@ public class StringManipulation {
 
     }
 
+    public static void main(String[] args) {
+//    testing();
+        System.out.println("Please enter something!");
+        Scanner sc = new Scanner (System.in);
+        String input = sc.nextLine();
+        System.out.printf("You entered %s\n", input);
+        if (StringUtils.isNumeric(input)){
+            System.out.println(input + " is a number.");
+        } else {
+            System.out.println(input + " is NOT a number.");
+        }
+        System.out.println("Flipped case : " + StringUtils.swapCase(input));
+        System.out.println("Reversed: " + StringUtils.reverse(input));
+
+
+    }
+
 
 }
 
-}
+
